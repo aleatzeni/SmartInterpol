532d38e5b879d35592d10284cdda8e913bf088e0
aleatzeni <alessia.atzeni.14@ucl.ac.uk>
2022-03-29 18:24:16 +0100

    Upload NiftyReg files in batches

---
+++ SmartInterpol/functions/NiftyReg/Linux_files/niftyreg/reg-lib/CMakeFiles/_reg_globalTrans.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-lib/cpu/_reg_globalTrans.cpp" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-lib/CMakeFiles/_reg_globalTrans.dir/cpu/_reg_globalTrans.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "NR_VERSION=\"1.5.61\""
+  "_USE_SSE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-lib"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-lib/cpu"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/nifti"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/third-party"
+  "third-party/eigen3"
+  "."
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/nrrd"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/nrrd/NrrdIO"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-lib/CMakeFiles/_reg_tools.dir/DependInfo.cmake"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-lib/CMakeFiles/_reg_maths.dir/DependInfo.cmake"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/nifti/CMakeFiles/reg_nifti.dir/DependInfo.cmake"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
