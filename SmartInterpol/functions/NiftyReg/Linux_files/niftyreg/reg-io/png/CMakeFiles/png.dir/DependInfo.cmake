e53f4b4eaedf146f3af7b1758b858cd698d6514f
aleatzeni <alessia.atzeni.14@ucl.ac.uk>
2022-03-29 16:41:20 +0100

    Upload NiftyReg files in batches

---
+++ SmartInterpol/functions/NiftyReg/Linux_files/niftyreg/reg-io/png/CMakeFiles/png.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/png.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/png.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngerror.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngerror.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngget.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngget.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngmem.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngmem.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngpread.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngpread.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngread.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngread.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngrio.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngrio.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngrtran.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngrtran.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngrutil.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngrutil.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngset.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngset.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngtrans.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngtrans.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngwio.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngwio.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngwrite.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngwrite.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngwtran.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngwtran.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510/pngwutil.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/png/CMakeFiles/png.dir/lpng1510/pngwutil.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "NR_VERSION=\"1.5.61\""
+  "_USE_SSE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-lib"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-lib/cpu"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/nifti"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/third-party"
+  "third-party/eigen3"
+  "."
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/nrrd"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/nrrd/NrrdIO"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
