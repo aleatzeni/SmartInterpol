0daebda3c771eaa5063cdfae1899752f4e52aff5
aleatzeni <alessia.atzeni.14@ucl.ac.uk>
2022-03-29 16:42:22 +0100

    Upload NiftyReg files in batches

---
+++ SmartInterpol/functions/NiftyReg/Linux_files/niftyreg/reg-io/zlib/CMakeFiles/z.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/adler32.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/adler32.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/compress.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/compress.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/crc32.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/crc32.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/deflate.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/deflate.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/gzio.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/gzio.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/infback.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/infback.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/inffast.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/inffast.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/inflate.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/inflate.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/inftrees.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/inftrees.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/trees.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/trees.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/uncompr.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/uncompr.c.o"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib/zutil.c" "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/build/reg-io/zlib/CMakeFiles/z.dir/zutil.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "NR_VERSION=\"1.5.61\""
+  "_USE_SSE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/zlib"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/png/lpng1510"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-lib"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-lib/cpu"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/nifti"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/third-party"
+  "third-party/eigen3"
+  "."
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/nrrd"
+  "/autofs/space/panamint_005/users/iglesias/software/niftyreg-kcl/src/reg-io/nrrd/NrrdIO"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
